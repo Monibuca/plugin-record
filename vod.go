ddfaa080208431cd35b6e043df89a4c099034ed9
dexter <178529795@qq.com>
2021-08-10 11:29:36 +0800

    VOD增加CORS

--- vod.go
+++ vod.go
@@ -5,9 +5,12 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+
+	. "github.com/Monibuca/utils/v3"
 )
 
 func VodHandler(w http.ResponseWriter, r *http.Request) {
+	CORS(w, r)
 	streamPath := r.RequestURI[5:]
 	filePath := filepath.Join(config.Path, streamPath)
 	if file, err := os.Open(filePath); err == nil {
